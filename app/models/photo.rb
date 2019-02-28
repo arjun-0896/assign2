729c440356095b3725b5743a1dc080d917afafae
shraddha-bhadauria <46631072+shraddha-bhadauria@users.noreply.github.com>
2019-02-28 14:36:48 -0500

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -32,16 +32,16 @@ end
 
 
 
-<!--
-class Photo < ApplicationRecord
-  belongs_to :tour
+#
+#class Photo < ApplicationRecord
+#  belongs_to :tour
 
-  has_attached_file :image
+# has_attached_file :image
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+#  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  validates_attachment :image, presence: true,
-                       content_type: { content_type: "image/jpeg" },
-                       size: { in: 0..10.megabytes }
-end
--->
+#  validates_attachment :image, presence: true,
+#                       content_type: { content_type: "image/jpeg" },
+#                       size: { in: 0..10.megabytes }
+#end
+
