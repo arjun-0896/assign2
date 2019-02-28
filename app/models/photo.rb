22cf5380245df0773fe9e35a32e356e3b6956ea5
shraddha-bhadauria <46631072+shraddha-bhadauria@users.noreply.github.com>
2019-02-28 14:36:14 -0500

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,38 @@
+
+class Photo < ApplicationRecord
+  belongs_to :tour, optional:  true
+  before_destroy :remember_id
+  after_destroy :remove_id_directory
+
+
+
+  has_attached_file :image
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates_attachment :image, presence: true,
+                       content_type: { content_type: "image/jpeg" },
+                       size: { in: 0..10.megabytes }
+
+
+  #mount_uploader :photo_name, PhotoUploader
+  #validates :photo_name, presence: true
+
+  protected
+  def remember_id
+    @id = id
+  end
+
+
+  def remove_id_directory
+    FileUtils.remove_dir("#{Rails.root}/public/uploads/tour_photo/#{@id}", :force => true)
+  end
+end
+
+
+
+
+<!--
 class Photo < ApplicationRecord
   belongs_to :tour
 
@@ -9,3 +44,4 @@ class Photo < ApplicationRecord
                        content_type: { content_type: "image/jpeg" },
                        size: { in: 0..10.megabytes }
 end
+-->
